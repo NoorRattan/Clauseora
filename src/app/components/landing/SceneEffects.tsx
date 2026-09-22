e637267add677806f233e9d7ea93b17326c51b30
Noor Rattan <jnoorrattan@gmail.com>
2026-09-22 18:59:20 +0530

    Remove driver-sensitive depth effect

--- src/app/components/landing/SceneEffects.tsx
+++ src/app/components/landing/SceneEffects.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { Bloom, DepthOfField, EffectComposer } from "@react-three/postprocessing";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 export default function SceneEffects() {
   return (
     <EffectComposer multisampling={0}>
+      {/* Bloom keeps the ambient highlight without the driver-sensitive DOF shader. */}
       <Bloom intensity={0.14} luminanceThreshold={1.5} mipmapBlur />
-      <DepthOfField focusDistance={0.07} focalLength={0.15} bokehScale={0.4} height={240} />
     </EffectComposer>
   );
 }
